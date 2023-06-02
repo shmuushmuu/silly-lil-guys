e49a7494565ba8172e77b0c803fde696e5a48aaa
Sam Craig <samfcraig@gmail.com>
2023-06-01 22:48:56 -0400

    made an array of objects for our upcoming DnD game

---
+++ Object-practice/script.js
@@ -0,0 +1,37 @@
+var dndParty = [Shove, Try, Robot, MagicMan];
+
+var Shove = {
+    player: "Shmuu",
+    race: "Human",
+    class: "Ranger",
+    subclass: "Gloom Stalker",
+    level: 5,
+    alignment: "lawful neutral"
+};
+
+var Try = {
+    player: "Samantha",
+    race: "Loxodon",
+    class: "Bard",
+    subclass: "College of lore",
+    level: 5,
+    alignment: "no clue dude"
+};
+
+var Robot = {
+    player: "Johnathan",
+    race: "Automaton",
+    class: "Artificer",
+    subclass: "Armorer",
+    level: 5,
+    alignment: "uh whatever he was designated, I guess"
+};
+
+var MagicMan = {
+    player: "Chris",
+    race: "Eladrin",
+    class: "Wizard",
+    subclass: "School of Chronurgy",
+    level: 5,
+    alignment: "Prolly chaotic something"
+};
