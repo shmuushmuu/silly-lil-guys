ed8e73b299600e4342678b362acc96892a1952c9
Sam Craig <samfcraig@gmail.com>
2023-06-01 22:10:00 -0400

    Practiced methods using parties of D&D characters

---
+++ Methods-D&D-Parties/script.js
@@ -0,0 +1,14 @@
+var zyeldicaParty = ["Lellimus", "Harmony", "Alrich", "Sunny"];
+var lellimusParty = ["Lellimus", "Zenya", "Karina", "Leon", "Duin"];
+var soldariParty = ["Luk", "Ennui", "Koraki", "Pita Chip", "Niklaus"];
+
+console.log(zyeldicaParty);
+console.log(lellimusParty);
+console.log(soldariParty);
+
+soldariParty.sort();
+console.log(soldariParty);
+
+var cityOfEnlightenmentParty = zyeldicaParty.slice(1);
+cityOfEnlightenmentParty.push("Warryn", "Adamina");
+console.log(cityOfEnlightenmentParty);
